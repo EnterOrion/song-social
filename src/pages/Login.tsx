827f93c4ec02a0e03d110f85abd16ebeeefe230c
EnterOrion <orionwesson@gmail.com>
2023-04-28 00:44:49 +0530

    Update styling on login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import vinylIcon from "../assets/images/vinyl.png";
-import loopIcon from "../assets/images/icons/loop.svg";
 
 const Login: FC = () => {
   return (
@@ -9,15 +8,10 @@ const Login: FC = () => {
         <h1 className="login-header">Music listening made social.</h1>
         <div className="form-container">
           <img className="login-icon" src={vinylIcon} alt="Vinyl Icon" />
-          <h2>
-            What have you been listening to on{" "}
-            <object
-              className="loop-icon alt-margin"
-              data={loopIcon}
-              type="image/svg+xml"
-            ></object>
-            ?
+          <h2 className="bottom-margin">
+            What have you been listening to on repeat?
           </h2>
+          <h2>Connect today!</h2>
           <form>
             <div className="button-section">
               <button className="spotify-button">Register with Spotify</button>
